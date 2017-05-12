a146f9741f0c3dd1459d1d90a1805f7d583400f5
Simon Rigard <rigard_s@epita.fr>
2017-05-12 18:33:42 +0200

    Change database for David

--- Yakaserver/Yakaserver/settings.py
+++ Yakaserver/Yakaserver/settings.py
@@ -88,15 +88,21 @@ WSGI_APPLICATION = 'Yakaserver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'yakabase',
+#        'USER': 'postgres',
+#        'PASSWORD': 'ouais',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'yakabase',
-        'USER': 'postgres',
-        'PASSWORD': 'ouais',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
