9880bc2ac1ca8b32fe03cb98cf87e1bfcb4ad34c
Simon Rigard <rigard_s@epita.fr>
2017-05-27 16:41:34 +0200

    Added userProfile and Recette models

--- Yakaserver/app/forms.py
+++ Yakaserver/app/forms.py
@@ -25,12 +25,3 @@ class RecipeForm(forms.Form):
     cuisson = forms.DurationField()
     ingredients =forms.CharField(widget=forms.Textarea)
     recette = forms.CharField(widget=forms.Textarea)
-
-    #auteur
-
-#
-# class ContactForm(forms.Form):
-#     sujet = forms.CharField(max_length=100)
-#     message = forms.CharField(widget=forms.Textarea)
-#     envoyeur = forms.EmailField(label="Votre adresse mail")
-#     renvoi = forms.BooleanField(help_text="Cochez si vous souhaitez obtenir une copie du mail envoye.", required=False)
--- Yakaserver/app/models.py
+++ Yakaserver/app/models.py
@@ -3,5 +3,21 @@ Definition of models.
 """
 
 from django.db import models
+from django.contrib.auth.models import User
+import hashlib
+
+class Recette(models.Model):
+    nom = models.CharField(max_length=100)
+    preparation = models.DurationField()
+    cuisson = models.DurationField()
+    ingredients = models.CharField(max_length=300)
+    recette = models.CharField(max_length=800)
+    creation_date = models.DateTimeField(auto_now=True, blank=True)
+    user = models.ForeignKey(User)
+
+ 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
 
 # Create your models here.
